f6b86c9bfe696f28f66cf22176db51ba350e6525
Elder Carvalho <eld.carv@gmail.com>
2021-07-17 15:56:40 -0300

    chore: :heavy_plus_sign: :hammer: add and configure eslint with airbnb styleguide

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,16 @@
-const path = require('path')
+const CracoAlias = require("craco-alias");
 
 module.exports = {
-  webpack: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    },
-  }
-}
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "options",
+        baseUrl: "./",
+        aliases: {
+          "@": "./src",
+        }
+      }
+    }
+  ]
+};
---
+++ src/components/FormSidebar/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const FormSidebar: React.FC = () => <div>teste</div>;
+
+export default FormSidebar;
