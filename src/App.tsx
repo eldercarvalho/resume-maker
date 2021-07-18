23ef75d767e430cacdecac453b528c1dfea7b6e0
Elder Carvalho <eld.carv@gmail.com>
2021-07-17 23:39:06 -0300

    refactor: :recycle: clean react initial template

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,6 @@
-import FormSidebar from '@/components/FormSidebar';
-import logo from './logo.svg';
-import './App.css';
-
 const App: React.FC = () => (
   <div className="App">
-    <header className="App-header">
-      <FormSidebar />
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
+    <h1>Resume Builder</h1>
   </div>
 );
 
