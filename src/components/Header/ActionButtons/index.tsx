6568759b75e5cc8cc036524424e5f828597a0bb6
Elder Carvalho <eld.carv@gmail.com>
2022-04-15 15:57:09 -0300

    feat: remove download button

--- src/components/Header/ActionButtons/index.tsx
+++ src/components/Header/ActionButtons/index.tsx
@@ -1,5 +1,5 @@
-import * as html2pdf from 'html2pdf.js';
-import { FiDownload, FiPrinter } from 'react-icons/fi';
+// import * as html2pdf from 'html2pdf.js';
+import { FiPrinter } from 'react-icons/fi';
 
 import Button from '@/components/base/Button';
 import Tooltip from '@/components/base/Tooltip';
@@ -8,18 +8,18 @@ import { useIntl } from 'react-intl';
 const ActionButtons: React.FC = () => {
   const intl = useIntl();
 
-  const handleDownloadClick = () => {
-    const resume = document.querySelector('.sheet');
+  // const handleDownloadClick = () => {
+  //   const resume = document.querySelector('.sheet');
 
-    const opt = {
-      margin: 0,
-      filename: 'myresume.pdf',
-      image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 2 },
-      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
-    };
-    html2pdf().from(resume).set(opt).save();
-  };
+  //   const opt = {
+  //     margin: 0,
+  //     filename: 'myresume.pdf',
+  //     image: { type: 'jpeg', quality: 0.98 },
+  //     html2canvas: { scale: 2 },
+  //     jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+  //   };
+  //   html2pdf().from(resume).set(opt).save();
+  // };
 
   const handlePrintClick = () => {
     window.print();
@@ -27,11 +27,11 @@ const ActionButtons: React.FC = () => {
 
   return (
     <>
-      <Tooltip text={intl.formatMessage({ id: 'header.button.download.text' })}>
+      {/* <Tooltip text={intl.formatMessage({ id: 'header.button.download.text' })}>
         <Button textOnly iconOnly onClick={handleDownloadClick}>
           <FiDownload size={20} />
         </Button>
-      </Tooltip>
+      </Tooltip> */}
 
       <Tooltip text={intl.formatMessage({ id: 'header.button.print.text' })}>
         <Button textOnly iconOnly onClick={handlePrintClick}>
